ea23c210e8b168d01443b0a1ec85e7bf15bbb965
Tala Yaseen <112306113+talamyaseen@users.noreply.github.com>
2025-06-23 08:14:12 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,7 @@
-// src/firebase/firebase.ts
+// src/lib/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import type { Messaging } from "firebase/messaging"; // <-- النوع هنا
 
 const firebaseConfig = {
   apiKey: "AIzaSyCJin09XkeKkgNeWwi9WcwS5nLCgkSWTlc",
@@ -9,17 +10,16 @@ const firebaseConfig = {
   storageBucket: "farmx-org.firebasestorage.app",
   messagingSenderId: "560112655531",
   appId: "1:560112655531:web:31316c4289249679922bee",
-  measurementId: "G-08959EBY7N"
+  measurementId: "G-08959EBY7N",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-let analytics = null;
-let messaging = null;
+let analytics: any = null;
+let messaging: Messaging | null = null; // 🔥 هذا المهم
 
 if (typeof window !== "undefined") {
-
   import("firebase/analytics").then(({ getAnalytics }) => {
     analytics = getAnalytics(app);
   });
